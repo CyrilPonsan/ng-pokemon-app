7dae468bd1a62fb077352ec96af7b2f0caa4d199
CyrilPonsan <cyril.ponsan@gmail.com>
2022-08-13 04:33:25 +0200

    router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { POKEMONS } from "./mock-pokemon-list";
-import { Pokemon } from "./pokemon";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
   styles: [],
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected!: Pokemon | any;
-
-  ngOnInit(): void {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string) {
-    const pokemon: Pokemon | any = this.pokemonList.find(
-      (pokemon) => pokemon.id === +pokemonId
-    );
-    if (pokemon) {
-      console.log(`Vous avez demandé le pokémon : ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    }
-    console.log(`Vous avez demandé un pokémon qui n'existe pas.`);
-  }
-}
+export class AppComponent {}
