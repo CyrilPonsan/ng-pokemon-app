5ec6115c2fb0f4e419bc41d4032eeed84cacf9df
CyrilPonsan <cyril.ponsan@gmail.com>
2022-08-13 03:15:24 +0200

    directives

---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, ElementRef, HostListener, Input } from "@angular/core";
+
+@Directive({
+  selector: "[pkmnBorderCard]",
+})
+export class BorderCardDirective {
+  //  alias
+  @Input("pkmnBorderCard") borderColor!: string;
+
+  //  sans alias : @Input() pkmnBorderCard!: string;
+
+  initialColor: string = "#f5f5f5";
+  defaultColor: string = "#009688";
+  defaultHeight: number = 180;
+
+  constructor(private el: ElementRef) {
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor);
+  }
+
+  @HostListener("mouseenter") onMouseEnter() {
+    this.setBorder(this.borderColor || this.defaultColor);
+  }
+
+  @HostListener("mouseleave") onMouseLeave() {
+    this.setBorder(this.initialColor);
+  }
+
+  private setHeight(height: number): void {
+    this.el.nativeElement.style.height = `${this.defaultHeight}px`;
+  }
+
+  private setBorder(color: string): void {
+    this.el.nativeElement.style.border = `solid 4px ${color}`;
+  }
+}
