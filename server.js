2f5b0d33cdf169929d3a4be6ba60b17a3dabe40e
CyrilPonsan <cyril.ponsan@gmail.com>
2022-08-14 17:12:22 +0200

    coucou

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ function requireHTTPS(req, res, next) {
 const express = require("express");
 const app = express();
 app.use(requireHTTPS);
-app.use(express.static("./dist/<name-on-package.json>"));
+
+app.use(express.static("./dist/frontend"));
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/<name-on-package.json>/" });
+  res.sendFile("index.html", { root: "dist/frontend/" });
 });
 app.listen(process.env.PORT || 8080);
